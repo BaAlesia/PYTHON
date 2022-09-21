f623ff088784d3317f2392209ab492d029695925
Alesia <105723411+BaAlesia@users.noreply.github.com>
2022-09-21 15:04:16 +0300

    Update HW 2.py
    + 2 варианта

--- HW/HW 2.py
+++ HW/HW 2.py
@@ -6,16 +6,32 @@
 333
 4444
 55555
+   
+Решение:
+for i in range (1, 6) :
+    print (str(i)*i)
     
+Решение (с пробелами):   
+for i in range(1, 6):
+    print((str(i) + ' ') * i)
+    
+Решение:  
+for i in range(1, 6):
+ for k in range(i):
+  print(i, end='')
+ print()
+    
+Решение:
+print(*[(str(i)+' ') *i for i in range(1, 6)], sep = '\n')
 
-Решение 1:
+Решение:
 print('''1
 22
 333
 4444
 55555''')
     
-Решение 2:  
+Решение:  
 a = 1
 b = 22
 c = 333
