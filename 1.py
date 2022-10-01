eece790de8ed80f75b05a0d464d9ba4420a44a56
Alesia <105723411+BaAlesia@users.noreply.github.com>
2022-10-01 09:20:41 +0300

    Add files via upload
    Start!

---
+++ 1.py
@@ -0,0 +1,28 @@
+print('Hello, world!')
+
+message = 'Hello, world!'
+print(message)
+
+message2 = 58
+print(message2)
+
+a = 2 + 2
+print(a)
+
+b = 5 - 2
+print(b)
+
+c = 24 / 2
+print(c)
+
+d = 2 * 22
+print(d)
+
+e = 6 ** 2
+print(e)
+
+f = 222 // 2
+print(f)
+
+e = 222 % 9
+print(e)
